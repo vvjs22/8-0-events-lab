ea90406a667278768b98c9ad6946917eb3a5a156
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-09-08 20:25:45 -0400

    Completed

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const currentColor = document.getElementById('current-color');
+const colors = document.getElementsByClassName('color');
+const cells = document.getElementsByClassName('cell');
+
+for (let color of colors) {
+  color.addEventListener('click', (event) => {
+    currentColor.style.background = event.target.style.background;
+  });
+}
+
+for (let cell of cells) {
+  cell.addEventListener('click', (event) => {
+    event.target.style.background = currentColor.style.background;
+  });
+}
+
+
+let button = document.createElement("button");
+button.textContent = "Reset";
+document.querySelector("#palette").append(button);
+
+button.addEventListener("click", () => {
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "white";
+    
+  });
+});
